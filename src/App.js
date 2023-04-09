bc75fa67d2abbab7e1a58cdf94a103b193a2ce9e
Kasimova Alina <kasimovaaalina@gmail.com>
2023-04-09 18:32:35 +0500

    Add title to gif

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>~Приложение работает~</h1>
-      <iframe src="https://giphy.com/embed/qIE7nKYuG4jZK" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/naruto-shippuden-sasuke-sakura-qIE7nKYuG4jZK">via GIPHY</a></p>
+      <iframe src="https://giphy.com/embed/qIE7nKYuG4jZK" title='naruto' width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/naruto-shippuden-sasuke-sakura-qIE7nKYuG4jZK">via GIPHY</a></p>
     </div>
   );
 }
