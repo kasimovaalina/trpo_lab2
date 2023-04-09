02639c253ff5c741238ee8f6c4875cc69aeaa247
Kasimova Alina <kasimovaaalina@gmail.com>
2023-04-09 18:47:57 +0500

    Change app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>~Приложение работает~</h1>
       <iframe src="https://giphy.com/embed/qIE7nKYuG4jZK" title='naruto' width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/naruto-shippuden-sasuke-sakura-qIE7nKYuG4jZK">via GIPHY</a></p>
+      <h2>Update</h2>
     </div>
   );
 }
