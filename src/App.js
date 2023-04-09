a3c342e001509f2897ebd2c75f5a17c6207903e1
Kasimova Alina <kasimovaaalina@gmail.com>
2023-04-09 18:20:58 +0500

    Edit typo:

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>~Приложение работает~</h1>
-      <iframe src="https://giphy.com/embed/ohT97gdpR40vK" width="480" height="288" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/naruto-chibi-ohT97gdpR40vK">via GIPHY</a></p>
+      <iframe title='naruto' src="https://giphy.com/embed/ohT97gdpR40vK" width="480" height="288" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/naruto-chibi-ohT97gdpR40vK">via GIPHY</a></p>
     </div>
   );
 }
