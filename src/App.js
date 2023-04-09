89b7abe17dc6f22fe511b80bdef19ef5961f0d0e
Kasimova Alina <kasimovaaalina@gmail.com>
2023-04-09 19:06:23 +0500

    Changed gif

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>~Приложение работает~</h1>
-      <iframe src="https://giphy.com/embed/qIE7nKYuG4jZK" title='naruto' width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/naruto-shippuden-sasuke-sakura-qIE7nKYuG4jZK">via GIPHY</a></p>
-      <h2>Update</h2>
+      <iframe title='pandas' src="https://giphy.com/embed/ieaUdBJJC19uw" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/panda-slide-ieaUdBJJC19uw">via GIPHY</a></p>
+      <h2>~~~</h2>
     </div>
   );
 }
