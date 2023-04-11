4adaa0b59d53e6ceea3d0d9e17a7db5d0a6746d6
Kasimova Alina <kasimovaaalina@gmail.com>
2023-04-11 19:50:15 +0500

    Add tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import MyComponent from './MyComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>~Приложение работает~</h1>
       <iframe title='pandas' src="https://giphy.com/embed/ieaUdBJJC19uw" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/panda-slide-ieaUdBJJC19uw">via GIPHY</a></p>
-      <h2>~~~</h2>
+      <MyComponent text="Testing!"></MyComponent>
     </div>
   );
 }
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import MyComponent from './MyComponent';
+
+test('renders component with correct text', () => {
+  const { getByText } = render(<MyComponent text="Hello, World!" />);
+  const textElement = getByText(/Hello, world!/i);
+  expect(textElement).toBeInTheDocument();
+});
